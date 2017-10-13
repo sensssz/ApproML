1544c4773cb18a16d6a91436c86b320cf527cfb7
Jiamin Huang <hjmsens@gmail.com>
2017-10-13 13:22:40 -0400

    Minor bug fix.

--- LinearRegressionSample.m
+++ LinearRegressionSample.m
@@ -1,9 +1,9 @@
-function [ params, time, gradient_value, epsilon, gamma_value ] = LinearRegressionSample( ftr, lbl, sample_rate, lambda, probablity )
+function [ params, time, gradient_value, error_bound ] = LinearRegressionSample( ftr, lbl, sample_rate, lambda, probablity )
   num_examples = size(lbl, 1);
   sample_size = num_examples * sample_rate;
   rand_perm = randperm(num_examples);
   fsample = ftr(rand_perm(1:sample_size), :);
   lsample = lbl(rand_perm(1:sample_size), :);
   [ params, time, gradient_value ] = LinearRegression(fsample, lsample, lambda);
-  [ epsilon, gamma_value ] = LinearRegressionSampleError(fsample, lsample, params, lambda, probablity);
+  [ error_bound ] = LinearRegressionSampleError(fsample, lsample, params, lambda, probablity);
 end
