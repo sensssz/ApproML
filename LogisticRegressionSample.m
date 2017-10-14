f2f32cd148abfbad02c1abfbefebafd78a1eb56e
Jiamin Huang <hjmsens@gmail.com>
2017-10-14 14:25:33 -0400

    Return all results from the script.

--- LogisticRegressionSample.m
+++ LogisticRegressionSample.m
@@ -1,6 +1,6 @@
 function [ params, time, gradient_value, error_bound, cc ] = LogisticRegressionSample( ftr, lbl, sample_rate, lambda, probability )
   num_examples = size(lbl, 1);
-  sample_size = num_examples * sample_rate;
+  sample_size = int32(num_examples * sample_rate);
   rand_perm = randperm(num_examples);
   fsample = ftr(rand_perm(1:sample_size), :);
   lsample = lbl(rand_perm(1:sample_size), :);
