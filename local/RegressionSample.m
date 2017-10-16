cf6667ab20b455137141fb142d47561906337db5
Jiamin Huang <hjmsens@gmail.com>
2017-10-16 16:20:53 -0400

    Fix sampling method.

--- local/RegressionSample.m
+++ local/RegressionSample.m
@@ -1,10 +1,15 @@
 function [ params, sampling_time, regression_time, gradient_value, error_bound, cc ] = RegressionSample( regression, sample_error, ftr, lbl, sample_rate, lambda, probability )
   tic;
   num_examples = size(lbl, 1);
-  sample_size = int32(num_examples * sample_rate);
-  rand_perm = randperm(num_examples);
-  fsample = ftr(rand_perm(1:sample_size), :);
-  lsample = lbl(rand_perm(1:sample_size), :);
+  if sample_rate < 1
+    sample_size = int32(num_examples * sample_rate);
+    rand_perm = randperm(num_examples, sample_size);
+    fsample = ftr(rand_perm, :);
+    lsample = lbl(rand_perm, :);
+  else
+    fsample = ftr;
+    lsample = lbl;
+  end
   sampling_time = toc;
   [ params, regression_time, gradient_value ] = regression(fsample, lsample, lambda);
   [ error_bound, cc ] = sample_error(fsample, lsample, params, lambda, probability);
