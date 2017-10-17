865d0ceb696d390f224090b114f1d1d2965e174d
Jiamin Huang <hjmsens@gmail.com>
2017-10-17 16:23:15 -0400

    Save galues.

--- local/RunExperiments.m
+++ local/RunExperiments.m
@@ -1,9 +1,9 @@
 lambda = 1e-6;
-[logistic_full_params, logistic_full_time, ~] = LogisticRegression(trainf, trainl, lambda);
-[linear_full_params, linear_full_time, ~] = LinearRegression(trainf, trainl, lambda);
+[logistic_full_params, logistic_full_time, logistic_full_gvalue] = LogisticRegression(trainf, trainl, lambda);
+[linear_full_params, linear_full_time, linear_full_gvalue] = LinearRegression(trainf, trainl, lambda);
 sampling_rates = [0.01 0.02 0.05 0.08 0.1 0.2 0.5 0.8];
-[logistic_params, logistic_sampling_times, logistic_training_times, ~, logistic_error_bounds, logistic_ccs] = CollectTrainingStat(@LogisticRegressionSample, trainf, trainl, sampling_rates);
-[linear_params, linear_sampling_times, linear_training_times, ~, linear_error_bounds, linear_ccs] = CollectTrainingStat(@LinearRegressionSample, trainf, trainl, sampling_rates);
+[logistic_params, logistic_sampling_times, logistic_training_times, logistic_gvalues, logistic_error_bounds, logistic_ccs] = CollectTrainingStat(@LogisticRegressionSample, trainf, trainl, sampling_rates);
+[linear_params, linear_sampling_times, linear_training_times, linear_gvalues, linear_error_bounds, linear_ccs] = CollectTrainingStat(@LinearRegressionSample, trainf, trainl, sampling_rates);
 logistic_training_times = [logistic_training_times logistic_full_time];
 linear_training_times = [linear_training_times linear_full_time];
 [logistic_prediction_errors, logistic_model_errors] = LogisticEmpiricalErrors(logistic_full_params, logistic_params, trainf);
