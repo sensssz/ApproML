2dafd2bf384affd9ac645a2b41f8e4b717bae2e2
Jiamin Huang <hjmsens@gmail.com>
2017-10-17 12:58:15 -0400

    Add sampling time.

--- local/CollectTrainingStat.m
+++ local/CollectTrainingStat.m
@@ -1,4 +1,4 @@
-function [ params, training_times, gvalues, error_bounds, ccs ] = CollectTrainingStat( regression, trainf, trainl, sampling_rates )
+function [ params, sampling_times, training_times, gvalues, error_bounds, ccs ] = CollectTrainingStat( regression, trainf, trainl, sampling_rates )
   num_sampling_rates = size(sampling_rates, 2);
   params = [];
   sampling_times = zeros(1, num_sampling_rates);
@@ -12,7 +12,7 @@ function [ params, training_times, gvalues, error_bounds, ccs ] = CollectTrainin
     sampling_rate = sampling_rates(1, i);
     [param, sampling_time, training_time, gvalue, error_bound, cc] = regression(trainf, trainl, sampling_rate, lambda, probability);
     params = [params param];
-    training_times(1, i) = sampling_time;
+    sampling_times(1, i) = sampling_time;
     training_times(1, i) = training_time;
     gvalues = [gvalues gvalue];
     error_bounds = [error_bounds, error_bound];
