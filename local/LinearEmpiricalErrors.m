55456495e3224007e5f218ca89cdf72a6eb9bb84
Jiamin Huang <hjmsens@gmail.com>
2017-10-17 14:41:15 -0400

    calculate the mean error for linear regression.

--- local/LinearEmpiricalErrors.m
+++ local/LinearEmpiricalErrors.m
@@ -2,12 +2,12 @@ function [ linear_prediction_errors, linear_model_errors ] = EmpiricalErrors( or
   original_prediction = testset * original_model;
   num_tests = size(testset, 1);
   num_models = size(models, 2);
-  linear_prediction_errors = [];
+  linear_prediction_errors = zeros(1, num_models);
   linear_model_errors = [];
   for i = 1:num_models
     model = models(:, i:i);
-    prediction = testset * model;
-    linear_prediction_errors = [linear_prediction_errors (prediction - original_prediction) ./ original_prediction];
-    linear_model_errors = [linear_model_errors (model - original_model) ./ original_model];
+    predictions = testset * model;
+    linear_prediction_errors(1, i) = mean((predictions - original_prediction) ./ original_prediction);
+    linear_model_errors = [linear_model_errors model - original_model ./ original_model];
   end
 end
