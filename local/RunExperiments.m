045fcb66fbb3ef542485af32fb6e3b6546e3df70
Jiamin Huang <hjmsens@gmail.com>
2017-10-18 16:38:14 -0400

    Separate logistic and linear.

--- local/RunExperiments.m
+++ local/RunExperiments.m
@@ -1,19 +1,27 @@
 lambda = 1e-6;
-[logistic_full_params, logistic_full_time, logistic_full_gvalue] = LogisticRegression(trainf, trainl, lambda);
-[linear_full_params, linear_full_time, linear_full_gvalue] = LinearRegression(trainf, trainl, lambda);
 % sampling_rates = [0.01 0.02 0.05 0.08 0.1 0.2 0.5 0.8];
 sampling_rates = [0.01 0.02 0.05 0.08 0.1];
-[logistic_params, logistic_sampling_times, logistic_training_times, logistic_gvalues, logistic_error_bounds, logistic_ccs] = CollectTrainingStat(@LogisticRegressionSample, trainf, trainl, sampling_rates);
-[linear_params, linear_sampling_times, linear_training_times, linear_gvalues, linear_error_bounds, linear_ccs] = CollectTrainingStat(@LinearRegressionSample, trainf, trainl, sampling_rates);
-logistic_plot_training_times = [mean(logistic_training_times, 3) logistic_full_time];
-linear_plot_training_times = [mean(linear_training_times, 3) linear_full_time];
-[logistic_prediction_errors_full_model, logistic_prediction_errors_truth, logistic_model_errors] = LogisticEmpiricalErrors(logistic_full_params, logistic_params, trainf, trainl);
-[linear_prediction_errors_full_model, linear_prediction_errors_truth, linear_model_errors] = LinearEmpiricalErrors(linear_full_params, linear_params, trainf, trainl);
-PlotTrainingTime([sampling_rates 1], logistic_plot_training_times, 'logistic');
-PlotTrainingTime([sampling_rates 1], linear_plot_training_times, 'linear');
-PlotPredictionError(sampling_rates, logistic_prediction_errors_full_model, 'Full Model', 'logistic_full_model');
-PlotPredictionError(sampling_rates, logistic_prediction_errors_truth, 'Ground Truth', 'logistic_ground_truth');
-PlotPredictionError(sampling_rates, linear_prediction_errors_full_model, 'Full Model', 'linear_full_model');
-PlotPredictionError(sampling_rates, linear_prediction_errors_truth, 'Ground Truth', 'linear_ground_truth');
-PlotModelError(sampling_rates, logistic_full_params, logistic_model_errors, 'logistic');
-PlotModelError(sampling_rates, linear_full_params, linear_model_errors, 'linear');
+
+if do_logistic
+  % Logistic regression
+  [logistic_full_params, logistic_full_time, logistic_full_gvalue] = LogisticRegression(trainf, trainl, lambda);
+  [logistic_params, logistic_sampling_times, logistic_training_times, logistic_gvalues, logistic_error_bounds, logistic_ccs] = CollectTrainingStat(@LogisticRegressionSample, trainf, trainl, sampling_rates);
+  logistic_plot_training_times = [mean(logistic_training_times, 3) logistic_full_time];
+  [logistic_prediction_errors_full_model, logistic_prediction_errors_truth, logistic_model_errors] = LogisticEmpiricalErrors(logistic_full_params, logistic_params, trainf, trainl);
+  PlotTrainingTime([sampling_rates 1], logistic_plot_training_times, 'logistic');
+  PlotPredictionError(sampling_rates, logistic_prediction_errors_full_model, 'Full Model', 'logistic_full_model');
+  PlotPredictionError(sampling_rates, logistic_prediction_errors_truth, 'Ground Truth', 'logistic_ground_truth');
+  PlotModelError(sampling_rates, logistic_full_params, logistic_model_errors, 'logistic');
+end
+
+if do_linear
+  % Linear regression
+  [linear_full_params, linear_full_time, linear_full_gvalue] = LinearRegression(trainf, trainl, lambda);
+  [linear_params, linear_sampling_times, linear_training_times, linear_gvalues, linear_error_bounds, linear_ccs] = CollectTrainingStat(@LinearRegressionSample, trainf, trainl, sampling_rates);
+  linear_plot_training_times = [mean(linear_training_times, 3) linear_full_time];
+  [linear_prediction_errors_full_model, linear_prediction_errors_truth, linear_model_errors] = LinearEmpiricalErrors(linear_full_params, linear_params, trainf, trainl);
+  PlotTrainingTime([sampling_rates 1], linear_plot_training_times, 'linear');
+  PlotPredictionError(sampling_rates, linear_prediction_errors_full_model, 'Full Model', 'linear_full_model');
+  PlotPredictionError(sampling_rates, linear_prediction_errors_truth, 'Ground Truth', 'linear_ground_truth');
+  PlotModelError(sampling_rates, linear_full_params, linear_model_errors, 'linear');
+end
