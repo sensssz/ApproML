905de156305ec99f03a50b25855c20d9635c5118
Jiamin Huang <hjmsens@gmail.com>
2017-10-18 12:43:07 -0400

    Change sampling rates.

--- local/RunExperiments.m
+++ local/RunExperiments.m
@@ -1,7 +1,8 @@
 lambda = 1e-6;
 [logistic_full_params, logistic_full_time, logistic_full_gvalue] = LogisticRegression(trainf, trainl, lambda);
 [linear_full_params, linear_full_time, linear_full_gvalue] = LinearRegression(trainf, trainl, lambda);
-sampling_rates = [0.01 0.02 0.05 0.08 0.1 0.2 0.5 0.8];
+% sampling_rates = [0.01 0.02 0.05 0.08 0.1 0.2 0.5 0.8];
+sampling_rates = [0.01 0.02 0.05 0.08 0.1];
 [logistic_params, logistic_sampling_times, logistic_training_times, logistic_gvalues, logistic_error_bounds, logistic_ccs] = CollectTrainingStat(@LogisticRegressionSample, trainf, trainl, sampling_rates);
 [linear_params, linear_sampling_times, linear_training_times, linear_gvalues, linear_error_bounds, linear_ccs] = CollectTrainingStat(@LinearRegressionSample, trainf, trainl, sampling_rates);
 logistic_plot_training_times = [mean(logistic_training_times, 3) logistic_full_time];
