111c2d6ee0247ac49ed42b6553aac3f4373ed52c
Jiamin Huang <hjmsens@gmail.com>
2017-10-18 23:26:02 -0400

    Fix prediction error ylabel.

--- local/PlotPredictionError.m
+++ local/PlotPredictionError.m
@@ -18,7 +18,7 @@ function PlotPredictionError( sampling_rates, prediction_errors, against, prefix
   set(gca,'XTick',linspace(1, num_sampling_rates, num_sampling_rates));
   set(gca, 'xticklabel', xticklabels);
   xlabel('Sampling Rate');
-  if (strcmp(prefix, 'logistic') == 1)
+  if (~contains(prefix, 'linear'))
     ylabel(['Prediction Error Against ', against ,' (%)']);
   else
     ylabel(['Prediction Error Against ', against]);
