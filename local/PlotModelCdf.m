0df63bbc5af27e32a8b06bb6f3fb7802b12057a6
Jiamin Huang <hjmsens@gmail.com>
2017-10-20 16:39:11 -0400

    Add PlotModelCdf.

---
+++ local/PlotModelCdf.m
@@ -0,0 +1,32 @@
+function PlotModelCdf( sampling_rates, original_model, models, prefix )
+  close all;
+  num_sampling_rates = size(sampling_rates, 2);
+  [~, sorted_indices] = sort(abs(original_model), 'descend');
+  for i = 1:3
+    original_index = sorted_indices(i, 1);
+    xvalues = [original_model(original_index, 1); original_model(original_index, 1)];
+    yvalues = [0; 1];
+    for j = 1:num_sampling_rates
+      param_values = models(original_index:original_index, j:j, :);
+      cdfplot(param_values);
+      hold on;
+      plot(xvalues, yvalues);
+      title([num2str(i), OrdinalSuffix(i), '-', num2str(sampling_rates(1, j))]);
+      saveas(gcf, strcat(prefix, '_cdf_', num2str(i), OrdinalSuffix(i), '_', num2str(sampling_rates(1, j)), '.png'));
+      close all;
+    end
+  end
+end
+
+function [ suffix ] = OrdinalSuffix( i )
+  switch i
+    case 1
+      suffix = 'st';
+    case 2
+      suffix = 'nd';
+    case 3
+      suffix = 'rd';
+    otherwise
+      suffix = 'th';
+  end
+end
