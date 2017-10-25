c3f045baf6a17b49bc76a82abb45b8d19707e2f2
Jiamin Huang <hjmsens@gmail.com>
2017-10-25 14:25:32 -0400

    Error fix in the split script.

--- local/Split.m
+++ local/Split.m
@@ -4,10 +4,10 @@ function [ trainf, trainl ] = Split( training_set, label_is_first )
   test_size = int32(data_size * 0.1);
   train_size = data_size - test_size;
   if label_is_first
-    trainf = [training_set(:, 2:feature_size) ones(train_size, 1)];
-    trainl = training_set(:, 1:1);
+    trainf = [training_set(1:train_size, 2:feature_size) ones(train_size, 1)];
+    trainl = training_set(train_size+1:data_size, 1:1);
   else
-    trainf = [training_set(:, 1:feature_size-1) ones(train_size, 1)];
-    trainl = training_set(:, feature_size:feature_size);
+    trainf = [training_set(1:train_size, 1:feature_size-1) ones(train_size, 1)];
+    trainl = training_set(train_size+1:data_size, feature_size:feature_size);
   end
 end
