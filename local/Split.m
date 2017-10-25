90edf624200e54715564971f8de08e235e270e29
Jiamin Huang <hjmsens@gmail.com>
2017-10-25 14:36:51 -0400

    Fix split script.

--- local/Split.m
+++ local/Split.m
@@ -6,12 +6,12 @@ function [ trainf, trainl, testf, testl ] = Split( training_set, label_is_first 
   if label_is_first
     trainf = [training_set(1:train_size, 2:feature_size) ones(train_size, 1)];
     trainl = training_set(1:train_size, 1:1);
-    testf = [training_set(train_size+1:data_size, 2:feature_size) ones(train_size, 1)];
+    testf = [training_set(train_size+1:data_size, 2:feature_size) ones(test_size, 1)];
     testl = training_set(train_size+1:data_size, 1:1);
   else
     trainf = [training_set(1:train_size, 1:feature_size-1) ones(train_size, 1)];
     trainl = training_set(1:train_size, feature_size:feature_size);
-    testf = [training_set(train_size+1:data_size, 1:feature_size-1) ones(train_size, 1)];
+    testf = [training_set(train_size+1:data_size, 1:feature_size-1) ones(test_size, 1)];
     testl = training_set(train_size+1:data_size, feature_size:feature_size);
   end
 end
