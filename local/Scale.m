904e8a46a93ed8f4e65f0145573f9ff340c2eaaa
Jiamin Huang <hjmsens@gmail.com>
2017-10-26 15:52:40 -0400

    Fix subscript error in Scale.m

--- local/Scale.m
+++ local/Scale.m
@@ -2,7 +2,7 @@ function [ training_set ] = Scale( training_set, original, count )
   [orows, ocols] = size(original);
   scaled = zeros(count * orows, ocols);
   for i = 1:count
-    scaled((i-1)*orows:i*orows, :) = original + bsxfun(@times, randn(size(original)), mean(original));
+    scaled((i-1)*orows+1:i*orows, :) = original + bsxfun(@times, randn(size(original)), mean(original));
   end
   training_set = [training_set; scaled];
 end
