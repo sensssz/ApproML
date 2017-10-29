12842e2294542fd3934e6659be077052f380fd36
Jiamin Huang <hjmsens@gmail.com>
2017-10-29 02:31:33 -0400

    Implement bootstrap.

---
+++ local/Bootstrap.m
@@ -0,0 +1,104 @@
+function [ params, sampling_times, training_times, model_error_bounds, prediction_error_bounds ] = Bootstrap( original_model, sampling_rates, lambda, regression, predict, trainf, trainl, testf, testl, prefix )
+  num_sampling_rates = size(sampling_rates, 2);
+  num_examples = size(trainf, 1);
+  num_features = size(trainf, 2);
+  params = zeros(num_features, num_sampling_rates);
+  sampling_times = zeros(1, num_sampling_rates);
+  training_times = zeros(1, num_sampling_rates);
+  model_error_bounds = zeros(num_features, 2, num_sampling_rates);
+  prediction_error_bounds = zeros(num_features, 2, num_sampling_rates);
+  for i = 1:num_sampling_rates
+    sample_rate = sampling_rates(1, i);
+    sample_size = int32(num_examples * sample_rate);
+    rand_perm = randperm(num_examples, sample_size);
+    fsample = trainf(rand_perm, :);
+    lsample = trainl(rand_perm, :);
+    [ param, training_time, model_error, prediction_error ] = BootstrapTrain(regression, predict, fsample, lsample, testf, lambda);
+    params(:, i:i) = param;
+    training_times(:, i:i) = training_time;
+    model_error_bounds(:, :, i:i) = model_error;
+    prediction_error_bounds(:, :, i:i) = prediction_error;
+  end
+
+  original_prediction = predict(original_model, testf);
+  PlotModelBoundingProbabilities(original_model, sampling_rates, model_error_bounds, prefix);
+  PlotPredictionBoundingProbabilities(original_prediction, sampling_rates, preidction_error_bounds, prefix);
+end
+
+function [ mean_param, training_time, model_error_bound, prediction_error_bound ] = BootstrapTrain( regression, predict, trainf, trainl, testf, lambda )
+  tic;
+  num_samples = size(trainf, 1);
+  num_features = size(trainf, 2);
+  num_tests = size(testf, 1);
+  num_runs = 1;
+  params = zeros(num_features, num_runs);
+  predictions = zeros(num_tests, num_runs);
+  for i = 1:num_runs
+    fsample = datasample(trainf, num_samples);
+    lsample = datasample(trainl, num_samples);
+    [ param, ~, ~ ] = regression(fsample, lsample, lambda);
+    params(:, i:i) = param;
+    predictions(:, i:i) = predict(testf, param);
+  end
+  training_time = toc;
+  mean_param = mean(params, 2);
+  model_error_bound = [ prctile(params, 1, 2) prctile(params, 99, 2) ];
+  prediction_error_bound = [ prctile(predictions, 1, 2) prctile(predictions, 99, 2) ];
+end
+
+function PlotModelBoundingProbabilities( original_model, sampling_rates, model_error_bounds, prefix )
+  num_models = size(model_error_bounds, 3);
+  bounding_probabilities = zeros(1, num_models);
+  for i = 1:num_models
+    error_bound = model_error_bounds(:, :, i:i);
+    min_model = error_bound(:, 1:1);
+    max_model = error_bound(:, 2:2);
+    if sum(min_model > original_model) == 0 && sum(max_model < original_model) == 0
+      bounding_probabilities(1, i) = bounding_probabilities(1, i) + 1;
+    end
+  end
+
+  num_sampling_rates = size(sampling_rates, 2);
+  plot(bounding_probabilities * 100 / num_runs);
+  xticklabels = cell(num_sampling_rates);
+  xticklabels = xticklabels(1, :);
+  for i = 1:num_sampling_rates
+      xticklabels{i} = strcat(num2str(sampling_rates(1, i) * 100), '%');
+  end
+  set(gca,'FontSize',22);
+  set(gca,'XTick',linspace(1, num_sampling_rates, num_sampling_rates));
+  set(gca, 'xticklabel', xticklabels);
+  xlabel('Sampling Rate');
+  ylabel({'Probability of Error Bound', 'Holding for Bootstrap Model (%)'});
+  figname = strcat(prefix, '_bootstrap_model_error_bound');
+  export_fig([figname,'.pdf'], '-pdf','-transparent');
+  close all;
+end
+
+function PlotPredictionBoundingProbabilities( original_prediction, sampling_rates, prediction_error_bounds, prefix )
+  num_sampling_rates = size(sampling_rates, 3);
+  bounding_probabilities = zeros(1, num_sampling_rates);
+  num_predictions = size(original_prediction, 1);
+  for i = 1:num_sampling_rates
+    error_bound = prediction_error_bounds(:, :, i:i);
+    min_prediction = error_bound(:, 1:1);
+    max_prediction = error_bound(:, 2:2);
+    success = sum(min_prediction <= original_prediction & max_prediction >= original_prediction);
+    bounding_probabilities(1, i) = success / num_predictions;
+  end
+
+  plot(bounding_probabilities * 100 / num_runs);
+  xticklabels = cell(num_sampling_rates);
+  xticklabels = xticklabels(1, :);
+  for i = 1:num_sampling_rates
+      xticklabels{i} = strcat(num2str(sampling_rates(1, i) * 100), '%');
+  end
+  set(gca,'FontSize',22);
+  set(gca,'XTick',linspace(1, num_sampling_rates, num_sampling_rates));
+  set(gca, 'xticklabel', xticklabels);
+  xlabel('Sampling Rate');
+  ylabel({'Probability of Error Bound', 'Holding for Bootstrap Model (%)'});
+  figname = strcat(prefix, '_bootstrap_prediction_error_bound');
+  export_fig([figname,'.pdf'], '-pdf','-transparent');
+  close all;
+end
--- local/PlotLogisticProbErrorBoundSize.m
+++ local/PlotLogisticProbErrorBoundSize.m
@@ -1,4 +1,9 @@
-function PlotLogisticProbErrorBoundSize( probabilities, models, error_bounds, testf )
+function PlotLogisticProbErrorBoundSize( deltas, models, error_bounds, testf )
+  PlotErrorBoundSize(deltas, models, error_bounds, testf);
+  PlotBoundingProbabilities(deltas, original_model, models, error_bounds, testf);
+end
+
+function PlotErrorBoundSize( deltas, models, error_bounds, testf )
   num_models = size(models, 2);
   abs_testf = abs(testf);
   error_bound_sizes = zeros(1, num_models);
@@ -10,19 +15,79 @@ function PlotLogisticProbErrorBoundSize( probabilities, models, error_bounds, te
     error_bound_sizes(1, i) = mean(squeeze(max_prediction - min_prediction));
   end
 
-  num_probabilities = size(probabilities, 2);
+  num_deltas = size(deltas, 2);
   plot(error_bound_sizes);
-  xticklabels = cell(num_probabilities);
+  xticklabels = cell(num_deltas);
   xticklabels = xticklabels(1, :);
-  for i = 1:num_probabilities
-    xticklabels{i} = strcat(num2str(probabilities(1, i) * 100), '%');
+  for i = 1:num_deltas
+    xticklabels{i} = strcat(num2str(deltas(1, i) * 100), '%');
   end
   set(gca,'FontSize',22);
-  set(gca,'XTick',linspace(1, num_probabilities, num_probabilities));
+  set(gca,'XTick',linspace(1, num_deltas, num_deltas));
   set(gca, 'xticklabel', xticklabels);
   xlabel('Value of \delta');
   ylabel({'Size of Error Bound'});
-  figname = 'logistic_prob_error_bound_size';
+  figname = 'logistic_delta_error_bound_size';
+  export_fig([figname,'.pdf'], '-pdf','-transparent');
+  close all;
+end
+
+function PlotBoundingProbabilities( original_model, deltas, models, error_bounds, testf )
+  num_tests = size(testf, 1);
+  num_models = size(models, 2);
+  num_runs = size(models, 3);
+  original_prediction = sigmoid(testf * original_model);
+  prediction_probabilities = zeros(num_runs, num_models);
+  undeterminable_probabilities = zeros(num_runs, num_models);
+  abs_testf = abs(testf);
+  for i = 1:num_runs
+    for j = 1:num_models
+      model = models(:, j:j, i:i);
+      error_bound = error_bounds(:, j:j, i:i);
+      min_prediction = sigmoid(testf * model - abs_testf * error_bound);
+      max_prediction = sigmoid(testf * model + abs_testf * error_bound);
+      determinable = (min_prediction < 0.5 & max_prediction < 0.5) |...
+                     (min_prediction > 0.5 & max_prediction > 0.5);
+      num_indeterminable = sum(~determinable);
+      undeterminable_probabilities(i, j) = num_indeterminable / num_tests;
+
+      correct = sum(determinable & min_prediction < 0.5 & original_prediction < 0.5) +...
+                sum(determinable & min_prediction > 0.5 & original_prediction > 0.5);
+      prediction_probabilities(i, j) = correct / (num_tests - num_indeterminable);
+    end
+  end
+
+  num_deltas = size(deltas, 2);
+
+  % Plot prediction probability
+  plot(mean(prediction_probabilities) * 100);
+  xticklabels = cell(num_deltas);
+  xticklabels = xticklabels(1, :);
+  for i = 1:num_deltas
+    xticklabels{i} = strcat(num2str(deltas(1, i) * 100), '%');
+  end
+  set(gca,'FontSize',22);
+  set(gca,'XTick',linspace(1, num_deltas, num_deltas));
+  set(gca, 'xticklabel', xticklabels);
+  xlabel('Sampling Rate');
+  ylabel({'Probability of Error Bound', 'Holding for Sample Prediction (%)'});
+  figname = 'logistic_delta_prediction_error_bound';
+  export_fig([figname,'.pdf'], '-pdf','-transparent');
+  close all;
+
+  % Plot undeterminable probability
+  plot(mean(undeterminable_probabilities) * 100);
+  xticklabels = cell(num_deltas);
+  xticklabels = xticklabels(1, :);
+  for i = 1:num_deltas
+    xticklabels{i} = strcat(num2str(deltas(1, i) * 100), '%');
+  end
+  set(gca,'FontSize',22);
+  set(gca,'XTick',linspace(1, num_deltas, num_deltas));
+  set(gca, 'xticklabel', xticklabels);
+  xlabel('Sampling Rate');
+  ylabel({'Probability of Prediction', 'Being Undeterminable (%)'});
+  figname = 'logistic_delta_undeterminable_probability';
   export_fig([figname,'.pdf'], '-pdf','-transparent');
   close all;
 end
